89aee53b07f1cb432185e3772528c755fcfd715e
edwardghp <edward.ortega340@gmail.com>
2025-04-26 12:17:37 -0500

    Optica Backend

--- src/main/java/com/sistema/optica/SistemaOpticaBackendApplication.java
+++ src/main/java/com/sistema/optica/SistemaOpticaBackendApplication.java
@@ -1,11 +1,23 @@
 package com.sistema.optica;
 
+import com.sistema.optica.entidades.Employee;
+import com.sistema.optica.entidades.Rol;
+import com.sistema.optica.excepciones.UsuarioFoundException;
+import com.sistema.optica.servicios.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SistemaOpticaBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public static void main(String[] args) {
 
@@ -14,6 +26,22 @@ public class SistemaOpticaBackendApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /*try {
+			Employee employee = new Employee();
+			employee.setNombres("Administrativo");
+            //employee.setApellidos("Apellido1");
+            employee.setUsername("aministrativo@gmail.com");
+            employee.setPassword(bCryptPasswordEncoder.encode("12345"));
+            employee.setTelefono("987654321");
+            employee.setPerfil("foto.png");
 
+			Rol rol = new Rol();
+			rol.setNombre("Administrativo");
+
+			Employee usuarioGuardado = employeeService.guardarEmpleado(employee,"Administrativo");
+			System.out.println(usuarioGuardado.getUsername());
+		} catch (UsuarioFoundException exception){
+			exception.printStackTrace();
+		}*/
     }
 }
